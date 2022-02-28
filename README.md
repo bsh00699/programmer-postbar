df96b5d4a3ed530c45961c8b11db22e4690f5f8d
bsh00699 <525534397@qq.com>
2022-02-28 23:48:37 +0800

    init env

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # programmer-postbar
-Use node + react + typeorm to build a community platform
+
+#### 介绍
+
+
+#### 安装PostgreSQL
+* [Mac OS 上安装 PostgreSQL](https://www.runoob.com/postgresql/mac-install-postgresql.html)
+#### 初始化环境
+```
+npm install typeorm -g
+npm init --database postgres
+
+npm install express
+npm install -D @types/express morgan @types/morgan
+npm start
+```
---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import express from 'express'
+import morgan from 'morgan'
+import authRoutes from './routes/auth'
+
+const app = express()
+
+app.use(express.json())
+app.use(morgan('dev'))
+app.use('/api/auth', authRoutes)
+
+app.get('/', (_, res) => {
+  res.send('hello postbar')
+})
+
+app.listen(3333, async () => {
+  console.log('🚀 Server running at http://localhost:3333');
+  try {
+    await createConnection()
+    console.log('Database connected!!!');
+  } catch (err) {
+    console.log('statr err: ', err)
+  }
+})
