d70b9df244e9b9ad56cf1af3cc32d15b6d56dc81
bsh00699 <525534397@qq.com>
2022-03-15 23:08:18 +0800

    debuger

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,11 +1,11 @@
 import { FormEvent, useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
-import Axios from 'axios'
-import { useRouter } from 'next/router'
+// import Axios from 'axios'
+// import { useRouter } from 'next/router'
 
-import InputGroup from '../components/InputGroup'
-import { useAuthState } from '../context/auth'
+// import InputGroup from '../components/InputGroup'
+// import { useAuthState } from '../context/auth'
 
 export default function Register() {
   const [email, setEmail] = useState('')
@@ -14,10 +14,10 @@ export default function Register() {
   const [agreement, setAgreement] = useState(false)
   const [errors, setErrors] = useState<any>({})
 
-  const { authenticated } = useAuthState()
+  // const { authenticated } = useAuthState()
 
-  const router = useRouter()
-  if (authenticated) router.push('/')
+  // const router = useRouter()
+  // if (authenticated) router.push('/')
 
   const submitForm = async (event: FormEvent) => {
     event.preventDefault()
@@ -26,55 +26,53 @@ export default function Register() {
       setErrors({ ...errors, agreement: 'You must agree to T&Cs' })
       return
     }
+    // try {
+    //   await Axios.post('/auth/register', {
+    //     email,
+    //     password,
+    //     username,
+    //   })
 
-    try {
-      await Axios.post('/auth/register', {
-        email,
-        password,
-        username,
-      })
-
-      router.push('/login')
-    } catch (err) {
-      setErrors(err.response.data)
-    }
+    //   router.push('/login')
+    // } catch (err) {
+    //   setErrors(err.response.data)
+    // }
   }
 
   return (
-    <div className="flex bg-white">
+    <div className="bg-white">
       <Head>
         <title>Register</title>
       </Head>
-
       <div
-        className="h-screen bg-center bg-cover w-36"
+        className="h-screen bg-center bg-no-repeat bg-cover"
         style={{
-          backgroundImage: `url('${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/images/work.jpg')`,
+          backgroundImage: `url('/images/work.jpg')`,
         }}
-      ></div>
-      <div className="flex flex-col justify-center pl-6">
-        <div className="w-70">
-          <h1 className="mb-2 text-lg font-medium">Sign Up</h1>
-          <p className="mb-10 text-xs">
-            By continuing, you agree to our User Agreement and Privacy Policy
+      >
+        <div className="flex pl-20 pt-80">
+          <div className="bg-white w-max">
+            <h1 className="mb-2 text-lg font-medium">Sign Up</h1>
+            <p className="mb-10 text-xs">
+              By continuing, you agree to our User Agreement and Privacy Policy
           </p>
-          <form onSubmit={submitForm}>
-            <div className="mb-6">
-              <input
-                type="checkbox"
-                className="mr-1 cursor-pointer"
-                id="agreement"
-                checked={agreement}
-                onChange={(e) => setAgreement(e.target.checked)}
-              />
-              <label htmlFor="agreement" className="text-xs cursor-pointer">
-                I agree to get emails about cool stuff on Readit
+            <form onSubmit={submitForm}>
+              <div className="mb-6">
+                <input
+                  type="checkbox"
+                  className="mr-1 cursor-pointer"
+                  id="agreement"
+                  checked={agreement}
+                  onChange={(e) => setAgreement(e.target.checked)}
+                />
+                <label htmlFor="agreement" className="text-xs cursor-pointer">
+                  I agree to get emails about cool stuff on Readit
               </label>
-              <small className="block font-medium text-red-600">
-                {errors.agreement}
-              </small>
-            </div>
-            <InputGroup
+                <small className="block font-medium text-red-600">
+                  {errors.agreement}
+                </small>
+              </div>
+              {/* <InputGroup
               className="mb-2"
               type="email"
               value={email}
@@ -97,18 +95,19 @@ export default function Register() {
               setValue={setPassword}
               placeholder="PASSWORD"
               error={errors.password}
-            />
+            /> */}
 
-            <button className="w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border border-blue-500 rounded">
-              Sign Up
+              <button className="w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border border-blue-500 rounded">
+                Sign Up
             </button>
-          </form>
-          <small>
-            Already a readitor?
+            </form>
+            <small>
+              Already a readitor?
             <Link href="/login">
-              <a className="ml-1 text-blue-500 uppercase">Log In</a>
-            </Link>
-          </small>
+                <a className="ml-1 text-blue-500 uppercase">Log In</a>
+              </Link>
+            </small>
+          </div>
         </div>
       </div>
     </div>
