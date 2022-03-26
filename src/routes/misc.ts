4e31c95060eeecf6cafcaefec08d417f824b1bdc
bsh00699 <525534397@qq.com>
2022-03-26 23:53:11 +0800

    feat: add top-subs router

--- src/routes/misc.ts
+++ src/routes/misc.ts
@@ -1,6 +1,8 @@
 import { Request, Response, Router } from "express";
+import { getConnection } from 'typeorm'
 import Comment from "../entities/Comments";
 import Post from "../entities/Post";
+import Sub from '../entities/Sub'
 import User from "../entities/User";
 import Vote from "../entities/Vote";
 import auth from "../middleware/auth";
@@ -14,7 +16,7 @@ const vote = async (req: Request, res: Response) => {
   }
   try {
     const user: User = res.locals.user
-    const post = await Post.findOneOrFail({ identifier, slug })
+    let post: Post = await Post.findOneOrFail({ identifier, slug })
     let vote: Vote | undefined
     let comment: Comment | undefined
     if (commentIdentifier) {
@@ -35,8 +37,20 @@ const vote = async (req: Request, res: Response) => {
       }
       await vote.save()
     } else if (value === 0) {
-
+      await vote.remove()
+    } else if (vote.value !== value) {
+      vote.value = value
+      await vote.save()
     }
+
+    post = await Post.findOneOrFail(
+      { identifier, slug },
+      { relations: ['comments', 'comments.votes', 'sub', 'votes'] }
+    )
+    post.setUserVote(user)
+    post.comments.forEach((c) => c.setUserVote(user))
+
+    return res.json(post)
 
   } catch (err) {
     console.log(err)
@@ -44,5 +58,39 @@ const vote = async (req: Request, res: Response) => {
   }
 }
 
+const topSubs = async (_: Request, res: Response) => {
+  try {
+    /**
+     * SELECT s.title, s.name,
+     * COALESCE('http://localhost:5000/images/' || s."imageUrn" , 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y') as imageUrl,
+     * count(p.id) as "postCount"
+     * FROM subs s
+     * LEFT JOIN posts p ON s.name = p."subName"
+     * GROUP BY s.title, s.name, imageUrl
+     * ORDER BY "postCount" DESC
+     * LIMIT 5;
+     */
+    const imageUrlExp = `COALESCE('${process.env.APP_URL}/images/' || s."imageUrn" , 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y')`
+    const subs = await getConnection()
+      .createQueryBuilder()
+      .select(
+        `s.title, s.name, ${imageUrlExp} as "imageUrl", count(p.id) as "postCount"`
+      )
+      .from(Sub, 's')
+      .leftJoin(Post, 'p', `s.name = p."subName"`)
+      .groupBy('s.title, s.name, "imageUrl"')
+      .orderBy(`"postCount"`, 'DESC')
+      .limit(5)
+      .execute()
+
+    return res.json(subs)
+  } catch (err) {
+    return res.status(500).json({ error: 'Something went wrong' })
+  }
+}
+
 const router = Router()
 router.post('/vote', auth, vote)
+router.get('/top-subs', topSubs)
+
+export default router
