9720ffda2686a6c1c713ba43e5fb9b631bf69ff7
bsh00699 <525534397@qq.com>
2022-04-29 22:47:27 +0800

    feat: add 404 page

---
+++ client/src/pages/404.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+
+const NotFound = () => {
+
+  return (
+    <div className='flex flex-col items-center'>
+      <div className="mt-10 mb-4 text-5xl text-gray-800">
+        Page Not Found
+      </div>
+      <Link href='/'>
+        <a href="" className="px-4 py-2">Go Home</a>
+      </Link>
+
+    </div>
+  )
+}
+
+export default NotFound
