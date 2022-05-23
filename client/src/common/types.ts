68da630eba67e374cbbf0c5448eb0da59081d515
bsh00699 <525534397@qq.com>
2022-05-23 23:56:36 +0800

    feat: add comment in client

--- client/src/common/types.ts
+++ client/src/common/types.ts
@@ -35,3 +35,13 @@ export interface Sub {
   postCount?: string
 }
 
+export interface Comment {
+  identifier: string
+  body: string
+  username: string
+  createdAt: string
+  updatedAt: string
+  post?: Post
+  userVote: number
+  voteScore: number
+}
--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -7,7 +7,7 @@ import useSWR from 'swr'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import classNames from 'classnames'
-import { Post } from '../../../../common/types'
+import { Post, Comment } from '../../../../common/types'
 import SideBar from '../../../../components/SideBar'
 import { useAuthState } from '../../../../ctx/auth'
 import ActionButton from '../../../../components/ActionButton'
@@ -23,6 +23,10 @@ const PostPage = () => {
     identifier && slug
       ? `/posts/${identifier}/${slug}`
       : null
+  )
+
+  const { data: comments, mutate } = useSWR<Comment[]>(
+    identifier && slug ? `/posts/${identifier}/${slug}/comments` : null
   )
 
   if (error) {
@@ -138,6 +142,55 @@ const PostPage = () => {
                 </div>
               )
             }
+            ////comments\\\\\
+            {comments?.map((comment) => (
+              <div className="flex" key={comment.identifier}>
+                {/* Vote section */}
+                <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+                  {/* Upvote */}
+                  <div
+                    className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                    onClick={() => vote(1, comment)}
+                  >
+                    <i
+                      className={classNames('icon-arrow-up', {
+                        'text-red-500': comment.userVote === 1,
+                      })}
+                    ></i>
+                  </div>
+                  <p className="text-xs font-bold">{comment.voteScore}</p>
+                  {/* Downvote */}
+                  <div
+                    className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-600"
+                    onClick={() => vote(-1, comment)}
+                  >
+                    <i
+                      className={classNames('icon-arrow-down', {
+                        'text-blue-600': comment.userVote === -1,
+                      })}
+                    ></i>
+                  </div>
+                </div>
+                <div className="py-2 pr-2">
+                  <p className="mb-1 text-xs leading-none">
+                    <Link href={`/u/${comment.username}`}>
+                      <a className="mr-1 font-bold hover:underline">
+                        {comment.username}
+                      </a>
+                    </Link>
+                    <span className="text-gray-600">
+                      {`
+                            ${comment.voteScore}
+                            points •
+                            ${dayjs(comment.createdAt).fromNow()}
+                          `}
+                    </span>
+                  </p>
+                  <p>{comment.body}</p>
+                </div>
+              </div>
+            ))}
+            /////
           </div>
         </div>
         {/* sidebar */}
