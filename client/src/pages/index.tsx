be69cc87609c36dabbfe0cbc82ebafc8d503a66f
bsh00699 <525534397@qq.com>
2022-05-26 22:50:31 +0800

    feat: add submit post func

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -52,18 +52,17 @@ export default function Home() {
                   return (
                     <div key={name} className='flex items-center px-4 py-2 text-xs border-b'>
                       {/* image */}
-                      <div className='mr-2 overflow-hidden rounded-full hover:cursor-pointer'>
-                        <Link href={`r/${name}`}>
-                          <Image
-                            src={imageUrl}
-                            alt='Sub'
-                            width={32}
-                            height={30}
-                          />
-                        </Link>
-                      </div>
                       <Link href={`r/${name}`}>
-                        <div className='font-bold hover:cursor-pointer'>
+                        <Image
+                          src={imageUrl}
+                          alt='Sub'
+                          width={32}
+                          height={32}
+                          className='rounded-full hover:cursor-pointer'
+                        />
+                      </Link>
+                      <Link href={`r/${name}`}>
+                        <div className='ml-2 font-bold hover:cursor-pointer'>
                           /r/{name}
                         </div>
                       </Link>
---
+++ client/src/pages/r/[sub]/submit.tsx
@@ -0,0 +1,82 @@
+import Axios from 'axios'
+import Head from "next/head"
+import { useRouter } from "next/router"
+import { FormEvent, useState } from "react"
+import useSWR from "swr"
+import { Post, Sub } from "../../../common/types"
+import SideBar from "../../../components/SideBar"
+
+
+const Submit = () => {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+
+  const router = useRouter()
+  const { sub: subName } = router.query
+  const { data: sub, error } = useSWR<Sub>(subName ? `/subs/${subName}` : null)
+
+  if (error) router.push('/')
+
+  const submitPost = async (e: FormEvent) => {
+    e.preventDefault()
+    if (title.trim() === '') return
+    try {
+      const { data: post } = await Axios.post<Post>('/posts', {
+        title: title.trim(),
+        body,
+        sub: sub.name
+      })
+      // 重定向
+      router.push(`/r/${sub.name}/${post.identifier}/${post.slug}`)
+    } catch (err) {
+      console.log('submit post err', err);
+    }
+  }
+
+  return (
+    <div className="container flex pt-5">
+      <Head>
+        <title>Submit to CrazyBar</title>
+      </Head>
+      <div className="mt-4 w-160">
+        <div className="p-4 bg-white rounded">
+          <div className="mb-3 text-lg">Submit a post to /r/{subName}</div>
+          <form onSubmit={submitPost}>
+            <div className="relative mb-2">
+              <input
+                type="text"
+                className='w-full px-3 py-2 border border-gray-300 rounded focus:outline-none'
+                maxLength={300}
+                value={title}
+                placeholder='Title'
+                onChange={e => setTitle(e.target.value)}
+              />
+              <div className="absolute mb-2 text-sm text-gray-500 select-none focus:border-gray-600"
+                style={{ top: 10, right: 10 }}
+              >
+                {title.trim().length} / 300
+              </div>
+            </div>
+            <textarea
+              className='w-full p-3 border border-gray-300 rounded focus:outline-none'
+              value={body}
+              placeholder='Text (optional)'
+              rows={4}
+              onChange={e => setBody(e.target.value)}
+            ></textarea>
+            <div className="flex justify-end">
+              <button type='submit' className="px-3 py-1 blue button"
+                disabled={title.trim().length === 0}
+              >Submit</button>
+            </div>
+
+          </form>
+        </div>
+      </div>
+      { sub && <SideBar sub={sub} />}
+    </div>
+  )
+}
+
+export default Submit
